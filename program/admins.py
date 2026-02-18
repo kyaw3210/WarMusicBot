93fcb5972d859d94326d55b30228d7b2a2aea52a
kyaw3210 <kmaykyaw1234@gmail.com>
2026-02-18 14:50:49 +0630

    Update admins.py

--- program/admins.py
+++ program/admins.py
@@ -1,12 +1,16 @@
-from pyrogram import filters
-from typing import List, Union
-from config import COMMAND_PREFIXES
-
-other_filters = filters.group & ~filters.edit_message & ~filters.via_bot & ~filters.forwarded
-other_filters2 = (
-    filters.private & ~filters.edit_message
+from cache.admins import admins
+from driver.veez import call_py
+from pyrogram import Client, filters
+from driver.decorators import authorized_users_only
+from driver.filters import command, other_filters, other_filters2
+from driver.queues import QUEUE, clear_queue
+from driver.utils import skip_current_song, skip_item
+from config import BOT_USERNAME, GROUP_SUPPORT, IMG_1, IMG_2, IMG_5
+from pyrogram.types import (
+    CallbackQuery,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    Message,
 )
 
-def command(commands: Union[str, List[str]]):
-    return filters.command(commands, COMMAND_PREFIXES)
-    
+other_filters = filters.group & ~filters.via_bot & ~filters.forwarded
