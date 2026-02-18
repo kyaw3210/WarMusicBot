da11da3ada6abf03474dc4a6e8f6b5d177b32003
kyaw3210 <kmaykyaw1234@gmail.com>
2026-02-18 18:53:27 +0630

    Fix formatting of song command decorator

--- program/downloader.py
+++ program/downloader.py
@@ -35,7 +35,7 @@ ydl_opts = {
 }
 
 
-@Client.on_message(command(["song", f"song@{bn}"]) 
+@Client.on_message(command(["song", f"song@{bn}"])) 
 async def song(_, message):
     query = " ".join(message.command[1:])
     m = message.reply("🔎 finding song...")
