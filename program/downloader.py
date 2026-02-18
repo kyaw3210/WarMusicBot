b48b06678f7f6eea243ff6300a14955d019e425a
kyaw3210 <kmaykyaw1234@gmail.com>
2026-02-18 18:29:09 +0630

    Update downloader.py

--- program/downloader.py
+++ program/downloader.py
@@ -35,44 +35,37 @@ ydl_opts = {
 }
 
 
-@Client.on_message(command(["song", f"song@{bn}"]))  query = " ".join(message.command[1:])
-    m = message.reply("🔎 finding song...")
+@Client.on_message(command(["song", f"song@{bn}"]))
+async def song_download(client, message):
+    query = " ".join(message.command[1:])
+    m = await message.reply("🔎 finding song...")
     ydl_ops = {"format": "bestaudio[ext=m4a]"}
-    try:
-        results = YoutubeSearch(query, max_results=1).to_dict()
-        link = f"https://youtube.com{results[0]['url_suffix']}"
-        title = results[0]["title"][:40]
-        thumbnail = results[0]["thumbnails"][0]
-        thumb_name = f"{title}.jpg"
-        thumb = requests.get(thumbnail, allow_redirects=True)
-        open(thumb_name, "wb").write(thumb.content)
-        duration = results[0]["duration"]
+   
+    try:  
+    results = YoutubeSearch(query, max_results=1).to_dict()
+    link = f"https://youtube.com{results[0]['url_suffix']}"
+    title = results[0]["title"][:40]     thumbnail = results[0]["thumbnails"][0]
+    thumb_name = "thumb.jpg"
+         
+    
+    thumb = requests.get(thumbnail, allow_redirects=True)
+    with open(thumb_name, "wb") as f:
+    f.write(thumb.content)
+             
+    duration = results[0]["duration"]
 
     except Exception as e:
-        m.edit("❌ song not found.\n\nplease give a valid song name.")
-        print(str(e))
-        return
-    m.edit("📥 downloading file...")
+    await m.edit("❌ song not found.\n\nplease give a valid song name.")
+    print(str(e))
+    return
+
+    await m.edit("📥 downloading file...")
     try:
-        with yt_dlp.YoutubeDL(ydl_ops) as ydl:
-            info_dict = ydl.extract_info(link, download=False)
-            audio_file = ydl.prepare_filename(info_dict)
-            ydl.process_info(info_dict)
-        rep = f"**🎧 Uploader @{bn}**"
-        secmul, dur, dur_arr = 1, 0, duration.split(":")
-        for i in range(len(dur_arr) - 1, -1, -1):
-            dur += int(float(dur_arr[i])) * secmul
-            secmul *= 60
-        m.edit("📤 uploading file...")
-        message.reply_audio(
-            audio_file,
-            caption=rep,
-            thumb=thumb_name,
-            parse_mode="md",
-            title=title,
-            duration=dur,
-        )
-        m.delete()
+    with yt_dlp.YoutubeDL(ydl_ops) as ydl:
+    info_dict = ydl.extract_info(link, download=False)
+    audio_file = ydl.prepare_filename(info_dict)
+    ydl.process_info(info_dict)
+({ mdelee()
     except Exception as e:
         m.edit("❌ error, wait for bot owner to fix")
         print(e)
