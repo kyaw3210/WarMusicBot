c8078712bbdc2041bcec9e08f9f5a42b62d5077a
kyaw3210 <kmaykyaw1234@gmail.com>
2026-02-18 14:41:50 +0630

    Update filters.py

--- driver/filters.py
+++ driver/filters.py
@@ -2,11 +2,8 @@ from pyrogram import filters
 from typing import List, Union
 from config import COMMAND_PREFIXES
 
-other_filters = filters.group & ~filters.edit_message & ~filters.via_bot & ~filters.forwarded
-other_filters2 = (
-    filters.private & ~filters.edit_message
-)
+other_filters = filters.group & ~filters.via_bot & ~filters.forwarded
+other_filters2 = filters.private
 
 def command(commands: Union[str, List[str]]):
     return filters.command(commands, COMMAND_PREFIXES)
-    ES)
